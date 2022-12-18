86e73a2b8214224e1e84acb89021587cfad599ae
Alejo <alejandromo1995@gmail.com>
2022-12-18 20:04:08 +0400

    about layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,48 @@
-import Layout from '../components/layout/Layout';
+import Layout from 'components/layout/Layout';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import AlejandroPic from 'public/img/alejandro.png';
 
 export default function About() {
   return (
     <Layout page="About">
-      <h2>test</h2>
+      <div className="w-full max-w-2xl mx-auto px-4 mb-10">
+        <div className="p-8 text-2xl">
+          <p className="mb-10">I’m Alejandro Morales, a Venezuelan electronics engineer turned into a programmer based in Dubai.</p>
+
+          <div className="bg-pine-tree mx-auto w-48 mb-10">
+            <div className="relative pb-full w-full">
+              <Image src={AlejandroPic.src} alt="Alejandro Morales" loading="lazy" fill={true} />
+            </div>
+          </div>
+
+          <p className="mb-10">I have 4+ years experience working in web development, using these technologies:</p>
+
+          <div className="mb-10">
+
+          </div>
+
+          <p className="mb-10">I’ve been able to work in projects as a freelancer, with agencies, with bootcamps, and  part of the team of a brand.</p>
+
+          <p className="mb-10">Always ready for the next challenging project to come.</p>
+
+          <div className="flex justify-between text-lg mb-10">
+            <Link href="/contact" className="btn">
+              Contact me
+            </Link>
+
+            <Link href="/work" className="btn">
+              Check my work
+            </Link>
+          </div>
+
+          <p>
+            This is the lastest design of my website,<br/> 
+            <Link href="https://v1.alexmo.com/" target="_blank" className="underline">See previous design</Link>.
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
